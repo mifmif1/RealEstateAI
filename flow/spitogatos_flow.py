d2d91f946553d07b489a6cac9b61265ae5d427ac
mifmif1 <michael.to.business@gmail.com>
2025-08-17 18:54:05 +0300

    422 error with polygon search

--- flow/spitogatos_flow.py
+++ flow/spitogatos_flow.py
@@ -12,6 +12,33 @@ class SpitogatosFlow:
         self._geopy_data_source = GeopyData()
         self._spitogatos_data_source = SpitogatosData()
 
+    def extend_excel_by_polygon(self, excel_path, location_tolerance: int = 100, sqm_tolerance: int = 10):
+        df = pd.read_excel(excel_path)
+        average_column = []
+        median_column = []
+
+        for index, row in df.iterrows():
+            coords = self._geopy_data_source.get_coords_from_adderss(row["address"])
+            search_rectangle = self._geopy_data_source.rectangle_from_point(start_point=coords,
+                                                                            radius_meters=location_tolerance)
+            assets = self._spitogatos_data_source.get_by_polygon(location=search_rectangle,
+                                                                  min_area=row["sqm"] - sqm_tolerance,
+                                                                  max_area=row["sqm"] + sqm_tolerance)
+            if assets:
+                assets_average = statistics.mean([asset.price / asset.sqm for asset in assets])
+                assets_median = statistics.median([asset.price / asset.sqm for asset in assets])
+            else:
+                assets_average = pd.NA
+                assets_median = pd.NA
+            average_column.append(assets_average)
+            median_column.append(assets_median)
+
+            sleep(3)  # bot sneaking
+        df['price/sqm'] = df['price'] / df['sqm']
+        df['comparison_average'] = average_column
+        df['comparison_median'] = median_column
+        df.to_excel('./new_polygon1.xlsx', index=False)
+
     def extend_excel(self, excel_path, location_tolerance: int = 100, sqm_tolerance: int = 10):
         df = pd.read_excel(excel_path)
         average_column = []
@@ -42,4 +69,4 @@ class SpitogatosFlow:
 
 if __name__ == '__main__':
     s = SpitogatosFlow()
-    s.extend_excel(r"../try.xlsx", location_tolerance=300)
+    s.extend_excel_by_polygon(r"../try.xlsx", location_tolerance=300)
--- utils/consts/apis.py
+++ utils/consts/apis.py
@@ -1,3 +1,3 @@
 class ApisConsts:
-    USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0"
-    # USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+    # USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0"
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
