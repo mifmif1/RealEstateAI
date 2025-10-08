7841a4c82aafe7d650acb876d2f861d666626d2f
mifmif1 <michael.to.business@gmail.com>
2025-10-08 13:38:02 +0300

    delete gemini scratch

--- data_source/eauctions_data.py
+++ data_source/eauctions_data.py
@@ -3,4 +3,4 @@ class Eacutions_data:
         self._URL = ""
 
     def get_report(self, id: str):
-        
+        ref = 'https://www.eauction.gr/Auction/GetFile?par1=1%2fxatBhece4%3d&par2=DpAzkeIVkwE%3d'
