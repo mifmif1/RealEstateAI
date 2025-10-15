85f927dbe4fc0678cfb90ce240795133a046cc64
mifmif1 <michael.to.business@gmail.com>
2025-10-15 22:44:48 +0300

    separation

--- flow/spitogatos_flow.py
+++ flow/spitogatos_flow.py
@@ -27,7 +27,7 @@ class SpitogatosFlow:
         self._spitogatos_data_source = SpitogatosData()
 
     @staticmethod
-    def _valuation_row(row, assets: List[Asset]):
+    def _get_valuation_row(row, assets: List[Asset]) -> float:
         assert 'level' in row.keys()
         assert 'sqm' in row.keys()
         assert 'new_state' in row.keys()
@@ -61,32 +61,9 @@ class SpitogatosFlow:
 
         return row_new_price
 
-    def add_avm(self, df: pd.DataFrame, row_conditions: Callable[[pd.DataFrame], bool], location_tolerance: int = 100,
-                sqm_tolerance: int = None):
-        pass
-
-    def _search_assets_for_row(self, row: pd.Series, location_tolerance: int = 100, sqm_tolerance: int = None):
-        assert 'coords' in row.keys()
-        assert 'sqm' in row.keys()
-
-        i = 0
-        assets = []
-        point = self._geopy_data_source.convert_location_to_lon_lat(row['coords'])
-        while i < 3 and isinstance(assets, list) and len(assets) < 5:
-            search_rectangle = self._geopy_data_source.rectangle_from_point(
-                start_point=point,
-                radius_meters=location_tolerance)
-            assets = self._spitogatos_data_source.get_by_location(location=search_rectangle,
-                                                                  min_area=max(0, row[
-                                                                      "sqm"] - sqm_tolerance) if sqm_tolerance else 30,
-                                                                  max_area=(row[
-                                                                                "sqm"] + sqm_tolerance) if sqm_tolerance else 200)
-            location_tolerance *= 1.5
-            i += 1
-        return assets, location_tolerance / 1.5
-
     @staticmethod
-    def _open_excel(excel_path: str, must_columns: List[str] = ['sqm', 'price']) -> pd.DataFrame:
+    def _open_excel(excel_path: str,
+                    must_columns: List[str]) -> pd.DataFrame:
         """
                 price, sqm, coords are columns in the Excel
                 """
@@ -113,43 +90,76 @@ class SpitogatosFlow:
         df['score'] = (df['price/sqm'] - df['comparison_average']) / df['comparison_std']
         return df
 
+    def _search_assets_for_row(self, row: pd.Series,
+                               location_tolerance: int = 100,
+                               sqm_tolerance: int = None):
+        assert 'coords' in row.keys()
+        assert 'sqm' in row.keys()
 
-    def add_spitogatos_comparison(self, excel_path, row_conditions: Callable[[pd.Series], bool],
-                                  location_tolerance: int = 100,
-                                  sqm_tolerance: int = None):
-        df = self._open_excel(excel_path, ['price', 'sqm', 'coords'])
+        i = 0
+        assets = []
+        point = self._geopy_data_source.convert_location_to_lon_lat(row['coords'])
+        while i < 3 and isinstance(assets, list) and len(assets) < 5:
+            search_rectangle = self._geopy_data_source.rectangle_from_point(
+                start_point=point,
+                radius_meters=location_tolerance)
+            assets = self._spitogatos_data_source.get_by_location(location=search_rectangle,
+                                                                  min_area=max(0, row[
+                                                                      "sqm"] - sqm_tolerance) if sqm_tolerance else 30,
+                                                                  max_area=(row[
+                                                                                "sqm"] + sqm_tolerance) if sqm_tolerance else 200)
+            location_tolerance *= 1.5
+            i += 1
+        return assets, location_tolerance / 1.5
+
+    def _add_spitogatos_comparison(self, df: pd.DataFrame,
+                                   row_conditions: Callable[[pd.Series], bool] = lambda row: False,
+                                   location_tolerance: int = 100, sqm_tolerance: int = None) -> pd.DataFrame:
+        for index, row in df.iterrows():  # no batching due to short data (around 5000 rows)
+            # coords = self._geopy_data_source.coords_from_address(row["address"])
+            logger.info(f"handling {row['UniqueCode']}")
+
+            if row_conditions(row):
+                continue
+
+            assets, location_tolerance = self._search_assets_for_row(row=row,
+                                                                     location_tolerance=location_tolerance,
+                                                                     sqm_tolerance=sqm_tolerance)
+            # todo: do it properly with exceptions
+            if assets == -1:
+                break
+
+            if assets:
+                assets_price_sqm = [asset.price / asset.sqm for asset in assets]
+                mean = statistics.mean(assets_price_sqm)
+                df.loc[index, 'comparison_average'] = mean
+                df.loc[index, 'comparison_median'] = statistics.median(assets_price_sqm)
+                df.loc[index, '#assets'] = len(assets)
+                df.loc[index, 'spitogatos_url'] = assets[0].url
+                # df.loc[
+                #     index, 'eauctions_url'] = f"https://www.eauction.gr/Home/HlektronikoiPleistiriasmoi?code={row['UniqueCode']}&sortAsc=true&sortId=1&conductedSubTypeId=1&page=1"
+                df.loc[index, 'searched_radius'] = location_tolerance
+                if len(assets) > 1:
+                    std = statistics.stdev(assets_price_sqm)
+                    df.loc[index, 'comparison_std'] = std
+                    if std != 0:
+                        df.loc[index, 'score'] = (row['price/sqm'] - mean) / std
+                df.loc[index, 'revaluation'] = self._get_valuation_row(row, assets)
+                logger.info(f"fetched {len(assets)} assets")
+        logger.info("finished, SAVING!")
+        return df
+
+    def expand_excel__spitogatos_comparison(self, excel_path,
+                                            must_columns: List[str],
+                                            row_conditions: Callable[[pd.Series], bool] = lambda row: False,
+                                            location_tolerance: int = 100,
+                                            sqm_tolerance: int = None):
+        df = self._open_excel(excel_path=excel_path, must_columns=must_columns)
+        df = self._prepare_df(df)
         try:
-            for index, row in df.iterrows():  # no batching due to short data (around 5000 rows)
-                # coords = self._geopy_data_source.coords_from_address(row["address"])
-                logger.info(f"handling {row['AM Portal']}")
-
-                if row_conditions(row):
-                    continue
-
-                assets, location_tolerance = self._search_assets_for_row(row=row,
-                                                                         location_tolerance=location_tolerance,
-                                                                         sqm_tolerance=sqm_tolerance)
-                if assets == -1:
-                    break
-
-                if assets:
-                    assets_price_sqm = [asset.price / asset.sqm for asset in assets]
-                    df.loc[index, 'comparison_average'] = statistics.mean(assets_price_sqm)
-                    df.loc[index, 'comparison_median'] = statistics.median(assets_price_sqm)
-                    df.loc[index, '#assets'] = len(assets)
-                    df.loc[index, 'spitogatos_url'] = assets[0].url
-                    # df.loc[
-                    #     index, 'eauctions_url'] = f"https://www.eauction.gr/Home/HlektronikoiPleistiriasmoi?code={row['UniqueCode']}&sortAsc=true&sortId=1&conductedSubTypeId=1&page=1"
-                    df.loc[index, 'searched_radius'] = location_tolerance
-                    if len(assets) > 1:
-                        df.loc[index, 'comparison_std'] = statistics.stdev(assets_price_sqm)
-                    logger.info(f"fetched {len(assets)} assets")
-
-            logger.info("finished, SAVING!")
-
+            self._add_spitogatos_comparison(df, row_conditions, location_tolerance, sqm_tolerance)
         except Exception as e:
             logger.error(f"something failed. SAVING!: {e}")
-
         finally:
             df.to_excel(f'{excel_path}_spitogatos_comparisson_{datetime.datetime.now().strftime("%d%m%Y-%H%M")}.xlsx',
                         index=False)
@@ -170,8 +180,7 @@ if __name__ == '__main__':
 
     # s.extend_excel(excel_path=r"../byhand/real.xlsb_spitogatos_comparisson_25092025-1551.xlsx",
     #                row_conditions=dovalue_conditions)
-    s.add_spitogatos_comparison(excel_path=r"../byhand/dvg_reo.xlsx_spitogatos_comparisson_09102025-1401.xlsx",
-                                row_conditions=lambda row: (False or
-                                                            not pd.isna(row['comparison_average']) or
-                                                            False
-                                                            ))
+    s.expand_excel__spitogatos_comparison(
+        excel_path=r"../byhand/real.xlsb",
+        must_columns=['sqm', 'price', 'coords'],
+        row_conditions=lambda row: (not pd.isna(row['comparison_average'])))
