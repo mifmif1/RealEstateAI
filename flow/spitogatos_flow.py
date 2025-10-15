de302ed2e39af2845df7259999fa548b74a04e0e
mifmif1 <michael.to.business@gmail.com>
2025-10-15 21:03:55 +0300

    separating

--- flow/spitogatos_flow.py
+++ flow/spitogatos_flow.py
@@ -26,10 +26,6 @@ class SpitogatosFlow:
         self._geopy_data_source = GeopyData()
         self._spitogatos_data_source = SpitogatosData()
 
-    def score_row(self, row):
-        # depends on the std, mean, median, min, max, amount, set the score
-        pass
-
     @staticmethod
     def _valuation_row(row, assets: List[Asset]):
         assert 'level' in row.keys()
@@ -100,17 +96,28 @@ class SpitogatosFlow:
         else:  # "xlsx" in excel_path[-5:]
             df = pd.read_excel(excel_path)
 
-        #todo replace with any
+        # todo replace with any
         for must_column in must_columns:
             assert must_column in df.columns
 
         return df
 
+    @staticmethod
+    def _prepare_df(df: pd.DataFrame) -> pd.DataFrame:
+        df['price/sqm'] = df['price'] / df['sqm']
+        return df
 
-    def add_spitogatos_comparison(self, excel_path, row_conditions: Callable[[pd.Series], bool], location_tolerance: int = 100,
+    @staticmethod
+    def _add_score(df: pd.DataFrame) -> pd.DataFrame:
+        # depends on the std, mean, median, min, max, amount, set the score
+        df['score'] = (df['price/sqm'] - df['comparison_average']) / df['comparison_std']
+        return df
+
+
+    def add_spitogatos_comparison(self, excel_path, row_conditions: Callable[[pd.Series], bool],
+                                  location_tolerance: int = 100,
                                   sqm_tolerance: int = None):
         df = self._open_excel(excel_path, ['price', 'sqm', 'coords'])
-        df['price/sqm'] = df['price'] / df['sqm']
         try:
             for index, row in df.iterrows():  # no batching due to short data (around 5000 rows)
                 # coords = self._geopy_data_source.coords_from_address(row["address"])
@@ -127,8 +134,7 @@ class SpitogatosFlow:
 
                 if assets:
                     assets_price_sqm = [asset.price / asset.sqm for asset in assets]
-                    mean = statistics.mean(assets_price_sqm)
-                    df.loc[index, 'comparison_average'] = mean
+                    df.loc[index, 'comparison_average'] = statistics.mean(assets_price_sqm)
                     df.loc[index, 'comparison_median'] = statistics.median(assets_price_sqm)
                     df.loc[index, '#assets'] = len(assets)
                     df.loc[index, 'spitogatos_url'] = assets[0].url
@@ -136,17 +142,13 @@ class SpitogatosFlow:
                     #     index, 'eauctions_url'] = f"https://www.eauction.gr/Home/HlektronikoiPleistiriasmoi?code={row['UniqueCode']}&sortAsc=true&sortId=1&conductedSubTypeId=1&page=1"
                     df.loc[index, 'searched_radius'] = location_tolerance
                     if len(assets) > 1:
-                        std = statistics.stdev(assets_price_sqm)
-                        df.loc[index, 'comparison_std'] = std
-                        if std != 0:
-                            df.loc[index, 'score'] = (row['price/sqm'] - mean) / std
+                        df.loc[index, 'comparison_std'] = statistics.stdev(assets_price_sqm)
                     logger.info(f"fetched {len(assets)} assets")
 
             logger.info("finished, SAVING!")
 
         except Exception as e:
             logger.error(f"something failed. SAVING!: {e}")
-
 
         finally:
             df.to_excel(f'{excel_path}_spitogatos_comparisson_{datetime.datetime.now().strftime("%d%m%Y-%H%M")}.xlsx',
@@ -170,6 +172,6 @@ if __name__ == '__main__':
     #                row_conditions=dovalue_conditions)
     s.add_spitogatos_comparison(excel_path=r"../byhand/dvg_reo.xlsx_spitogatos_comparisson_09102025-1401.xlsx",
                                 row_conditions=lambda row: (False or
-                                               not pd.isna(row['comparison_average']) or
-                                               False
-                                               ))
+                                                            not pd.isna(row['comparison_average']) or
+                                                            False
+                                                            ))
