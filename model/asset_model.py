fc76a29dd503bc65876c503de58da3b5a28ac35f
mifmif1 <michael.to.business@gmail.com>
2026-03-03 18:01:56 +0200

    asset

--- model/asset_model.py
+++ model/asset_model.py
@@ -1,18 +1,16 @@
 from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 from model.geographical_model import Point
 
 
 class Asset(BaseModel):
-
-    location: Point
+    id: str
+    lon: float
+    lat: float
     sqm: float
     price: float
     url: Optional[str] = None
     level: Optional[int] = None
-    address: Optional[str] = None
-    new_state: Optional[bool] = None
-    searched_radius: Optional[float] = None
-    revaluated_price_meter: Optional[float] = None
     construction_year: Optional[int] = None
